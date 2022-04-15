48097df4ada901ba6844846c52af35d077e2de5d
jonny201251 <jonny201251@163.com>
2022-04-15 17:33:51 +0800

    a

--- src/main/java/com/haiying/project/Test.java
+++ src/main/java/com/haiying/project/Test.java
@@ -1,12 +1,17 @@
 package com.haiying.project;
 
 import cn.hutool.core.text.TextSimilarity;
+import cn.hutool.core.util.NumberUtil;
 
 public class Test {
     public static void main(String[] args) {
         String db = "七室光学红外材料实验室";
-        String page = "我是一只小小鸟";
+        String page = "七室光学红外材料实验室-我是一只小小鸟";
         double d = TextSimilarity.similar(db, page);
         System.out.println(d);
+        int d2= NumberUtil.round(d*100,0).intValue();
+        System.out.println(d2);
+//        Integer d22=d2.intValue();
+        System.out.println(d2 + "%");
     }
 }
