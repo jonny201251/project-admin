835d6910eb53fb90f226038465b85f19fc827acc
jonny201251 <jonny201251@163.com>
2022-07-05 06:31:35 +0800

    a

--- src/main/java/com/haiying/project/controller/ProcessInstController.java
+++ src/main/java/com/haiying/project/controller/ProcessInstController.java
@@ -43,6 +43,7 @@ public class ProcessInstController {
     WorkFlowBean workFlowBean;
     @Autowired
     HttpSession httpSession;
+
     //待办任务
     @PostMapping("myList")
     public IPage<ProcessInst> list() {
