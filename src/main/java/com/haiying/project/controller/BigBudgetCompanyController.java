65b9cdbdab75ed439c8fe12f682fc8af5cbe1177
jonny201251 <jonny201251@163.com>
2023-03-24 16:20:44 +0800

    a

--- src/main/java/com/haiying/project/controller/BigBudgetCompanyController.java
+++ src/main/java/com/haiying/project/controller/BigBudgetCompanyController.java
@@ -29,6 +29,7 @@ import java.util.Map;
 @RequestMapping("/bigBudgetCompany")
 @Wrapper
 public class BigBudgetCompanyController {
+
     @Autowired
     BigBudgetCompanyService bigBudgetCompanyService;
 
