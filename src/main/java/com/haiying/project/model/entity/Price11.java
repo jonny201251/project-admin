d8d39f35fcd7b46342777271aa8720c490f53bcb
jonny201251 <jonny201251@163.com>
2023-09-11 20:43:55 +0800

    aa

--- src/main/java/com/haiying/project/model/entity/Price11.java
+++ src/main/java/com/haiying/project/model/entity/Price11.java
@@ -18,7 +18,6 @@ import java.io.Serializable;
 @Getter
 @Setter
 public class Price11 implements Serializable {
-
     private static final long serialVersionUID = 1L;
 
     @TableId(value = "id", type = IdType.AUTO)
